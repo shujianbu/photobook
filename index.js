295c93720aefcb4549e885ee24d38aafc434db5b
Shujian Bu <shujian.bu@gmail.com>
2018-02-04 13:44:24 -0500

    setup basic vue app

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
-
 const Hapi = require('hapi');
 const Inert = require('inert');
 
-const server = new Hapi.Server({ port: 3000, host: 'localhost' });
+const server = new Hapi.Server({ host: 'localhost', port: 3000 });
 
 const init = async () => {
   await server.register(Inert);
 
   server.route({
     method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return h.file('./client/index.html');
+    path: '/{file*}',
+    handler: {
+      directory: {
+        path: 'client/'
+      }
     }
   });
 
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Photo Book'
+      }
+    }
+  }
+</script>
