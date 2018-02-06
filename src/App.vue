4a173ffed29cdb715c5fdbef525d4dd9917f7633
Shujian Bu <shujian.bu@gmail.com>
2018-02-06 14:56:21 -0500

    Prettier config

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data () {
-      return {
-        msg: 'Photo Book'
-      }
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Photo Book'
     }
   }
+}
 </script>
