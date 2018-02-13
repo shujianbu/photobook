4376e3cffc7d1d3048611a2d0b8461b836affea7
Shujian Bu <shujian.bu@gmail.com>
2018-02-13 10:04:08 -0500

    add config vars for Heroku deployment

--- config.js
+++ config.js
@@ -6,10 +6,11 @@ module.exports = {
   DBX_OAUTH_DOMAIN: 'https://www.dropbox.com',
   DBX_OAUTH_PATH: '/oauth2/authorize',
   DBX_TOKEN_PATH: '/oauth2/token',
-  DBX_APP_KEY: config.parsed.DBX_APP_KEY,
-  DBX_APP_SECRET: config.parsed.DBX_APP_SECRET,
-  OAUTH_REDIRECT_URL: 'http://localhost:3000/oauthredirect',
+  DBX_APP_KEY: process.env.DBX_APP_KEY || config.parsed.DBX_APP_KEY,
+  DBX_APP_SECRET: process.env.DBX_APP_SECRET || config.parsed.DBX_APP_SECRET,
+  OAUTH_REDIRECT_URL:
+    process.env.OAUTH_REDIRECT_URL || 'http://localhost:3000/oauthredirect',
   DBX_LIST_FOLDER_PATH: '/2/files/list_folder',
   DBX_LIST_FOLDER_CONTINUE_PATH: '/2/files/list_folder/continue',
-  DBX_GET_TEMPORARY_LINK_PATH: '/2/files/get_temporary_link'
+  DBX_GET_TEMPORARY_LINK_PATH: '/2/files/get_temporary_link',
 }
